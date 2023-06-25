be271626b29395ac560274a1894702439751e246
MELODICXP <CARMELOGONZALEZ1312@GMAIL.COM>
2023-06-25 17:43:05 -0500

    Integrate ChartJS to render chart of voting results

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 // ***** GLOBALS *****
 let productArray = [];
 let votingRounds = 25;
+let indexArray = [];
 
 // ***** DOM WINDOWS *****
 let imgContainer = document.getElementById('image-container');
@@ -14,10 +15,11 @@ let figCaptionOne = document.querySelector('#image-one + figcaption');
 let figCaptionTwo = document.querySelector('#image-two + figcaption');
 let figCaptionThree = document.querySelector('#image-three + figcaption');
 let resultsBtn = document.getElementById('show-results-btn');
-let resultsList = document.getElementById('results-container');
 let roundsLeft = document.getElementById('show-rounds-left');
 let resultsHeader = document.getElementById('results-header');
 
+// ***** CANVAS ELEMENT *****
+let ctx = document.getElementById('my-chart');
 
 // ***** CONSTRUCTOR FUNCTION *****
 function Product (name, imageExtension = 'jpg') {
@@ -39,33 +41,99 @@ function capitalizeFirstLetter(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-// Make bold text by creating element
-function createStrongElement(text) {
-  let strongElement = document.createElement('strong');
-  strongElement.textContent = text;
-  strongElement.style.fontWeight = 'bold';
-  return strongElement;
-}
-
 // Create Product Objects
 function createProduct(name, imageExtension ='jpg') {
   productArray.push(new Product(name, imageExtension));
 }
 
+// Create Chart from Chart.JS library
+function renderChart() {
+  // Prepare chart data
+  let chartLabels = [];
+  let chartViews =[];
+  let chartVotes = [];
+
+  for(let i = 0; i < productArray.length; i++) {
+    chartLabels.push(productArray[i].name);
+    chartViews.push(productArray[i].views);
+    chartVotes.push(productArray[i].votes);
+  }
+
+  // Configure chart options
+  let chartOptions = {
+    plugins: {
+      legend: {
+        labels: {
+          font: {
+            size: 14 // set font size for chart legends
+          }
+        }
+      }
+    },
+    // Set bar chart horizontally
+    indexAxis: 'y',
+    maintainAspectRatio: false,
+    scales: {
+      y: {
+        grid: {
+          display: true
+        }
+      },
+      x: {
+        grid: {
+          display: false
+        }
+      }
+    }
+  };
+
+  // Configure chart datasets
+  let chartDatasets = [
+    {
+      label: '# of Views',
+      data: chartViews,
+      borderWidth: 0.75,
+      borderColor: '#000000',
+      backgroundColor: '#7fffd4'
+    },
+    {
+      label: '# of Votes',
+      data: chartVotes,
+      borderWidth: 0.75,
+      borderColor: '#000000',
+      backgroundColor: '#ffa07a'
+    }
+  ];
+
+  // Create chart object
+  let chartConfig = {
+    type: 'bar',
+    data: {
+      labels: chartLabels,
+      datasets: chartDatasets
+    },
+    options: chartOptions
+  };
+
+  // Render chart
+  new Chart(ctx, chartConfig);
+}
+
 // Render Images
 function renderImages(){
-
-  // Generate three random images and check if images are unique
   let imageOneIndex, imageTwoIndex, imageThreeIndex;
 
-  do {
-    imageOneIndex = randomIndexGenerator();
-    imageTwoIndex = randomIndexGenerator();
-    imageThreeIndex = randomIndexGenerator();
-  } while (
-    imageOneIndex === imageTwoIndex ||
-    imageOneIndex === imageThreeIndex ||
-    imageTwoIndex === imageThreeIndex);
+  // Generate 6 random numbers and check if numbers are unique
+  while(indexArray.length < 6) {
+    let randomNumber = randomIndexGenerator();
+    if (!indexArray.includes(randomNumber) ) {
+      indexArray.push(randomNumber);
+    }
+  }
+  // Remove first three indices from indexArray
+  imageOneIndex = indexArray.shift();
+  imageTwoIndex = indexArray.shift();
+  imageThreeIndex = indexArray.shift();
 
   // Assign unique image as source to be rendered, assign title, assign alt
   imageOne.src = productArray[imageOneIndex].image;
@@ -75,12 +143,12 @@ function renderImages(){
 
   imageTwo.src = productArray[imageTwoIndex].image;
   imageTwo.title = productArray[imageTwoIndex].name;
-  imageOne.alt = `picture of ${productArray[imageTwoIndex].name}`;
+  imageTwo.alt = `picture of ${productArray[imageTwoIndex].name}`;
   figCaptionTwo.textContent = capitalizeFirstLetter(productArray[imageTwoIndex].name);
 
   imageThree.src = productArray[imageThreeIndex].image;
   imageThree.title = productArray[imageThreeIndex].name;
-  imageOne.alt = `picture of ${productArray[imageThreeIndex].name}`;
+  imageThree.alt = `picture of ${productArray[imageThreeIndex].name}`;
   figCaptionThree.textContent = capitalizeFirstLetter(productArray[imageThreeIndex].name);
 
   // Increase image views
@@ -98,17 +166,15 @@ function handleImgClick(event) {
   for(let i = 0; i < productArray.length; i++) {
     if(imageClicked === productArray[i].name){
       productArray[i].votes++;
+      // Generate new images
+      renderImages();
+      // Decrement voting round
+      votingRounds--;
+      roundsLeft.textContent = `There are ${votingRounds} voting rounds left!`;
     }
   }
 
-  // Generate new images
-  renderImages();
-
-  // Decrement voting round
-  votingRounds--;
-  roundsLeft.textContent = `There are ${votingRounds} voting rounds left!`;
-
-  // Once voting rounds equal zero, remove ability to click
+  // Once voting rounds equal zero, remove ability to click image
   if (votingRounds === 0){
     imgContainer.removeEventListener('click', handleImgClick);
   }
@@ -118,20 +184,17 @@ function handleShowResults() {
   if (votingRounds === 0) {
     resultsHeader.textContent = 'Voting Results';
 
-    // Create li elements to show results of rounds (view and votes)
-    for (let i = 0; i < productArray.length; i++) {
-      let resultListItem = document.createElement('li');
+    // Apply styling to .chart-container
+    let chartContainer = document.querySelector('.chart-container');
+    chartContainer.style.position = 'relative';
+    chartContainer.style.margin = 'auto';
+    chartContainer.style.height = '100vh';
+    chartContainer.style.width = '30vw';
+    chartContainer.style.border = '2px solid black';
+    chartContainer.style.background = 'white';
 
-      // Make votes and views bold and capitalize first letter of product
-      let productName = createStrongElement( capitalizeFirstLetter(productArray[i].name) );
-      let productVotes = createStrongElement(productArray[i].votes);
-      let productViews = createStrongElement(productArray[i].views);
-
-      // .innerHTML used since manipulating elements to make bold, and not .textContent
-      resultListItem.innerHTML = `${productName.outerHTML} - Votes: ${productVotes.outerHTML} & Views: ${productViews.outerHTML}`;
-
-      resultsList.appendChild(resultListItem);
-    }
+    // Create Chart
+    renderChart();
 
     // Prevent duplicate list re-creation
     resultsBtn.removeEventListener('click', handleShowResults);
@@ -162,5 +225,6 @@ createProduct('wine-glass');
 renderImages();
 
 imgContainer.addEventListener('click', handleImgClick);
+
 resultsBtn.addEventListener('click', handleShowResults);
 
